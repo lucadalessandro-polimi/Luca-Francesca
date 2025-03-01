0edb256f6901542891e1e46be670b8022e6f4764
francesca1606 <francesca.zambetti@mail.polimi.it>
2025-03-01 16:05:35 +0000

    remove_edge, polygon and triangle in delaunay

--- fdaPDE/src/geometry/delaunay.h
+++ fdaPDE/src/geometry/delaunay.h
@@ -6,38 +6,43 @@
 
 namespace fdapde {
 
+using LocalDim = 2;
+using EmbedDim = 2; 
+using coords_t = Eigen::Matrix<double, LocalDim, 1>;
+using node_t = DCEL<LocalDim, EmbedDim>::node_t;
+using halfedge_t = DCEL<LocalDim, EmbedDim>::halfedge_t;
+using cell_t = DCEL<LocalDim, EmbedDim>::cell_t;
 
-
-
-
-    //DA GENERALIZZARE A N-DIM (TUTTO)
-    bool is_point_inside_triangle(const Eigen::Vector2d& P, const Eigen::Vector2d& A, 
-        const Eigen::Vector2d& B, const Eigen::Vector2d& C) {
-// Calcoliamo le coordinate baricentriche
-Eigen::Matrix2d M;
-M << (B - A), (C - A); //CAPIRE IL RIEMPIEMENTO
-Eigen::Vector2d lambda = M.inverse() * (P - A); //COSTO COMPUTAZIONE CON INVERSO RISPETTO AL CILCO FOR 
-double lambda1 = lambda.x();
-double lambda2 = lambda.y();
-double lambda3 = 1 - lambda1 - lambda2;
-
-return (lambda1 >= 0 && lambda2 >= 0 && lambda3 >= 0);  //DEVE STARE TRA ZERO E UNO I COEFFICIENTI
+bool is_point_inside_triangle(const node_t& node_P, const node_t& node_A, const node_t& node_B, const node_t& node_C)}{
+    // baricenter test
+    coords_t P= node_P.coords();
+    coords_t A= node_A.coords();
+    coords_t B= node_B.coords();
+    coords_t C= node_C.coords();
+    Eigen::Matrix<double, LocalDim, LocalDim> M;
+    M << (B - A), (C - A); 
+    coords_t lambda = M.inverse() * (P - A);
+    double lambda1 = lambda.x();
+    double lambda2 = lambda.y();
+    double lambda3 = 1 - lambda1 - lambda2;
+    return (lambda1 >= 0 && lambda1<=1 && lambda2 >= 0 && lambda2<=1 && lambda3 >= 0 && lambda3<=1);  
 }
 
-// GLI PASSI UN FLAG PER SCARTARE LE DIMENSIONI MAGGIORI DI 3?
-// Funzione per verificare se un punto D è dentro il circumcerchio del triangolo ABC
-bool InCircle(const Eigen::Vector2d& A, const Eigen::Vector2d& B, const Eigen::Vector2d& C, const Eigen::Vector2d& D) {
-Eigen::Matrix4d M;
-M << A.x(), A.y(), A.squaredNorm(), 1,
-B.x(), B.y(), B.squaredNorm(), 1,
-C.x(), C.y(), C.squaredNorm(), 1,
-D.x(), D.y(), D.squaredNorm(), 1;
-
-// Calcoliamo il determinante della matrice
-double det = M.determinant();
-
-return det > 0; // Se positivo, il punto D è dentro il circumcerchio
+bool InCircle(const node_t& node_A, const node_t& node_B, const node_t& node_C, const node_t& node_D){
+    
+    coords_t A= node_A.coords();
+    coords_t B= node_B.coords();
+    coords_t C= node_C.coords();
+    coords_t D= node_D.coords();
+    
+    Eigen::Matrix<double, LocalDim+1, LocalDim+1> M;
+    M << (A-D).x(), (A-D).y(), (A-D).squaredNorm(), (B-D).x(), (B-D).y(), (B-D).squaredNorm(), (C-D).x(), (C-D).y(), (C-D).squaredNorm();
+    double det = M.determinant();
+    // if det is positive, D is inside A-B-C circumcircle
+    return det > 0; 
 }
+
+
 
 // Funzione per trovare il triangolo adiacente dall'altro lato di un edge
 DCEL<2,2>::cell_t* Adjacent(DCEL<2,2>::halfedge_t* edge) {
@@ -48,22 +53,117 @@ return edge->twin()->cell();
 return nullptr; // Se non ha twin, significa che il lato è sul bordo
 }
 
+
+//Da inserire in DCEL come duale di insert_edge (non specifico per triangoli ma per poligoni --> serve anche make_triangle?)
+halfedge_t* remove_edge(halfedge_t* v1){
+    if (v1->on_boundary()) { // v1 is on the boundary
+        //2 cases: 
+        //a. v1 and its next/prev are on boundary --> elongate v1->next or prev (SE NO NON PERMETTERE)
+        if(v1->next->on_boundary() || v1->prev()->on_boundary()){
+            v1->next()->set_prev(v1->prev());
+            v1->prev()->set_next(v1->next());
+            v1->next()->set_node(v1->node());     
+            halfedge_t* v2= v1->twin();
+            v2->next()->set_prev(v2->prev());
+            v2->prev()->set_next(v2->next());
+            v2->next()->set_node(v2->node());
+            // remove edges, cell is the same so it doesn't need to be removed
+            halfedge_t* next= v1->next();
+            n_halfedges_ -= 2;
+            auto it1 = std::find(halfedges_.begin(), halfedges_.end(), v1);
+            auto it2 = std::find(halfedges_.begin(), halfedges_.end(), v2);
+            halfedges_.erase(it1);
+            halfedges_.erase(it2); 
+            return next;
+        }
+        //b. only v1 is on boundary and its next/prev isn't
+        else{
+            halfedge_t* v2= v1->twin();
+            v1->next()->set_prev(v2->prev());
+            v2->prev()->set_next(v1->next());    
+            v1->prev()->set_next(v2->next());
+            v2->next()->set_prev(v1_prev());
+            // remove edges and cell
+            n_cells_--;
+            auto it = std::find(cells_.begin(), cells_.end(), v1->cell());
+            cells_.erase(it);
+            halfedge_t* next= v1->next();
+            n_halfedges_ -= 2;
+            auto it1 = std::find(halfedges_.begin(), halfedges_.end(), v1);
+            auto it2 = std::find(halfedges_.begin(), halfedges_.end(), v2);
+            halfedges_.erase(it1);
+            halfedges_.erase(it2); 
+            return next; 
+        }
+    } 
+
+    halfedge_t* v2 = v1->twin();
+    // set next of v1_prev to v2_next
+    v1->prev()->set_next(v2->next());      
+    v2->next()->set_prev(v1->prev());
+    // set next of v2_prev to v1_next
+    v2->prev()->set_next(v1->next());
+    v1->next()->set_prev(v2->prev());
+    // remove v2's cell
+    n_cells_--;
+    auto it = std::find(cells_.begin(), cells_.end(), v2->cell());
+    cells_.erase(it);    
+    // insert halfedges in v1's cell
+    halfedge_t* end = v2, begin = v2->next();
+    cell_t* c1 = v1->cell();
+    c1->set_halfedge(begin); // either this or v1->prev()
+    do {
+        begin->set_cell(c1);
+        begin = begin->next();
+    } while (begin != end);
+    // remove v1 and v2
+    halfedge_t* next= v1->next();
+    n_halfedges_ -= 2;
+    auto it1 = std::find(halfedges_.begin(), halfedges_.end(), v1);
+    auto it2 = std::find(halfedges_.begin(), halfedges_.end(), v2);
+    halfedges_.erase(it1);
+    halfedges_.erase(it2);  
+    return next;
+}
+
+halfedge_t* cell_on_boundary(cell_t* cell){  //metodo di cell_t
+    halfedge_t* h1 = cell->halfedge();
+    do{
+        if(h1->on_boundary()) return h1;
+        h1 = h1->next();
+    }while(h1!=cell->halfedge())
+    return nullptr;
+}
+
+// va inserita in DCEL come duale di make_polygon (deve rimuovere tutti i lati interni e creare una cavità)
+void remove_polygon(const cell_t* cell){ 
+
+    
+    halfedge_t* b = cell->on_boundary();
+    if (b) // only remove edges on boundary    DOVREBBE ARRIVARE FINO A CASO b. IN CUI RIMANE SOLO 1 LATO SUL BORDO
+      do{
+        b = remove_edge(b);
+      }while(b->on_boundary())
+      return; 
+    }
+
+    halfedge_t* h1 = cell->halfedge();
+    halfedge_t* ending = h1->twin()->next();
+    do{
+        h1 = remove_edge(h1);
+    }while(h1!=ending)
+
+    // remove cells --> already done by remove_edge  IN TEORIA, RICONTROLLARE
+}
+
+
+
 // Funzione per eliminare un triangolo dalla DCEL
-void DeleteTriangle(DCEL<2,2>& dcel, DCEL<2,2>::cell_t* triangle) {
-if (!triangle) return; // Se il triangolo è nullo, non facciamo nulla
+void delete_triangle(DCEL<LocalDim,EmbedDim>& dcel, cell_t* triangle) {
+if (!triangle) return; // 
 
-// Otteniamo gli half-edge del triangolo
-DCEL<2,2>::halfedge_t* h1 = triangle->halfedge();
-DCEL<2,2>::halfedge_t* h2 = h1->next();
-DCEL<2,2>::halfedge_t* h3 = h2->next();
+dcel.remove_polygon(triangle);
 
-// DA IMPLEMENTARE 
-dcel.remove_cell(triangle);
-
-// DA IMPLEMENTARE REMOVE_HALFEDGE
-if (h1->twin() == nullptr) dcel.remove_edge(h1);
-if (h2->twin() == nullptr) dcel.remove_edge(h2);
-if (h3->twin() == nullptr) dcel.remove_edge(h3);
 }
 
 
